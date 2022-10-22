5c660d65e0c6e58a865822eee558f0db1cb37a09
kromate <akpananthony33@gmail.com>
2022-10-22 21:09:24 +0100

    feat: update

--- src/helper/data.ts
+++ src/helper/data.ts
@@ -14,6 +14,7 @@ interface taskType {
 	title: string;
 	desc: string;
 	level: levelType;
+	assigne:string;
 }
 
 export const dummyData: boardType = {
@@ -24,7 +25,15 @@ export const dummyData: boardType = {
 			id: '1',
 			title: 'Demo Task one',
 			desc: 'This is a demo task one',
-			level: 3
+			level: 1,
+			assigne: 'Kromate'
+		},
+		{
+			id: '1',
+			title: 'Demo Task one',
+			desc: 'This is a demo task one',
+			level: 3,
+			assigne: 'Kromate'
 		}
 	],
 	todo: [
@@ -32,19 +41,22 @@ export const dummyData: boardType = {
 			id: '2',
 			title: 'Demo Task two',
 			desc: 'This is a demo task two',
-			level: 1
+			level: 1,
+			assigne: 'Kromate'
 		},
 		{
 			id: '3',
 			title: 'Demo Task three',
 			desc: 'This is a demo task three',
-			level: 2
+			level: 2,
+			assigne: 'Kromate'
 		},
 		{
 			id: '4',
 			title: 'Demo Task four',
 			desc: 'This is a demo task four',
-			level: 3
+			level: 3,
+			assigne: 'Kromate'
 		}
 	],
     inprogress: [
@@ -52,13 +64,15 @@ export const dummyData: boardType = {
 			id: '5',
 			title: 'Demo Task five',
 			desc: 'This is a demo task five',
-			level: 3
+			level: 3,
+			assigne: 'Kromate'
 		},
 		{
 			id: '6',
 			title: 'Demo Task six',
 			desc: 'This is a demo task six',
-			level: 2
+			level: 2,
+			assigne: 'Kromate'
 		}
     ],
 	done: []
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<header>
 		<h1 class="board-title">
-			This is a Kanban
+			{{ dummyData.title }}
 		</h1>
 	</header>
 	<main class="grid grid-cols-4 gap-6 justify-center items-start">
@@ -10,10 +10,10 @@
 				<h1 class="column-title">
 					Backlog
 				</h1>
-				<icon name="note" class="column-card-header-icon" />
+				<icon name="add" class="column-card-header-icon" />
 			</header>
 
-			<div class="column-card">
+			<div v-for="card in dummyData.backlog" :key="card.id" :class="[cardColorPicker(card.level), 'column-card']">
 				<icon name="edit" class="column-card-icon" />
 				<h3 class="column-card-title">
 					Twilio integration
@@ -28,27 +28,53 @@
 			</div>
 		</article>
 		<article class="column">
-			<h1 class="column-title">
-				To do
-			</h1>
+			<header class="flex items-center justify-between w-full">
+				<h1 class="column-title">
+					To do
+				</h1>
+				<icon name="add" class="column-card-header-icon" />
+			</header>
 		</article>
 		<article class="column">
-			<h1 class="column-title">
-				In progress
-			</h1>
+			<header class="flex items-center justify-between w-full">
+				<h1 class="column-title">
+					In progress
+				</h1>
+				<icon name="add" class="column-card-header-icon" />
+			</header>
 		</article>
 		<article class="column">
-			<h1 class="column-title">
-				Done
-			</h1>
+			<header class="flex items-center justify-between w-full">
+				<h1 class="column-title">
+					Done
+				</h1>
+				<icon name="add" class="column-card-header-icon" />
+			</header>
 		</article>
 	</main>
 </template>
 
 <script setup lang="ts">
+import { dummyData } from '@/helper/data'
+
 definePageMeta({
 	layout: 'default'
 })
+
+const cardColorPicker = (data:number) => {
+	console.log(data)
+	switch (data) {
+		case 1:{
+			return 'bg-red'
+		}
+		case 2:{
+			return 'bg-purple'
+		}
+		case 3:{
+			return 'bg-green'
+		}
+	}
+}
 </script>
 
 <style scoped></style>
