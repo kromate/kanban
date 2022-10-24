e041623af63f96ffa1b3912b73219a7e140a8ae5
kromate <akpananthony33@gmail.com>
2022-10-24 13:32:30 +0100

    feat: adding necessary amenities

---
+++ src/pages/board/_id.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		{{ $route.param }}
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/demo.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<header class="flex flex-col px-4">
+			<h1 class="board-title">
+				{{ dummyData.title }}
+			</h1>
+			<h3 class="board-desc">
+				{{ dummyData.desc }}
+			</h3>
+		</header>
+		<main class="grid grid-cols-4 gap-6 justify-center items-start">
+			<Column v-for="column in columns" :key="column.title" :title="column.title" :cards="dummyData[column.id]" @change="updateData" />
+		</main>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { dummyData, keys } from '@/helper/data'
+const updateData = (e) => {
+const changedArray = keys[e.type]
+dummyData.value[changedArray] = e.data
+}
+
+const columns = [
+	{ title: 'Backlog', id: 'backlog' },
+	{ title: 'To do', id: 'todo' },
+	{ title: 'In progress', id: 'inprogress' },
+	{ title: 'Done', id: 'done' }
+]
+
+definePageMeta({
+	layout: 'default'
+})
+
+</script>
+
+<style scoped></style>
